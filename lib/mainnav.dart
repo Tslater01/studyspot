e204f50a4f2dee5b566cba5e09a78b948b3f3622
nadiazmartinez <90414546+nadiazmartinez@users.noreply.github.com>
2023-03-01 08:31:11 -0800

    Add files via upload

---
+++ lib/mainnav.dart
@@ -0,0 +1,42 @@
+import 'package:appmainmenu/Navbar.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: NavBar(),
+      appBar: AppBar(
+        title: Text('SideBar'),
+      ),
+      body: Center(),
+    );
+  }
+}
