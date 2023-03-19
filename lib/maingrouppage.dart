9e1597ae4d3706e4473d49e395408aa4a041fd5d
kuhlinger <90282249+kuhlinger@users.noreply.github.com>
2023-03-19 16:35:10 -0700

    finished group pages

---
+++ lib/maingrouppage.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/page-1/createAGroup.dart';
+import 'package:myapp/page-1/navbar.dart';
+
+Color darkgreen = Color(0xFF20DF7F);
+
+class GroupsPage extends StatefulWidget {
+  @override
+  _GroupsPageState createState() => _GroupsPageState();
+}
+
+class _GroupsPageState extends State<GroupsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Study Spot'),
+        backgroundColor: darkgreen,
+      ),
+      drawer: NavBar(),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.network(
+                'https://calbaptist.edu/_resources/images/Logo_CBU-wordmark-stacked-positive.png',
+                width: 300,
+                height: 200,
+              ),
+              const Padding(
+                padding: EdgeInsets.only(right: 200.0),
+                child: Text(
+                  'Your Groups',
+                  style: TextStyle(
+                    fontFamily: 'Lexend Deca',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                    color: Color(0xFF224957),
+                  ),
+                ),
+              ),
+              SizedBox(height: 25),
+              SizedBox(
+                width: 350,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Color(0xFF224957)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                  child: const Text(
+                    'Physics',
+                    style: TextStyle(
+                      fontFamily: 'Lexend Deca',
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              SizedBox(
+                width: 350,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Color(0xFF224957)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                  child: const Text(
+                    'Roommates',
+                    style: TextStyle(
+                      fontFamily: 'Lexend Deca',
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              SizedBox(
+                width: 350,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Color(0xFF224957)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                  child: const Text(
+                    'EGR123',
+                    style: TextStyle(
+                      fontFamily: 'Lexend Deca',
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              SizedBox(
+                width: 350,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Color(0xFF224957)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                  child: const Text(
+                    'General',
+                    style: TextStyle(
+                      fontFamily: 'Lexend Deca',
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 40),
+              const Text(
+                'Create Group',
+                style: TextStyle(
+                  fontFamily: 'Lexend Deca',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                  color: Color(0xFF224957),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => FourTextBoxes()),
+                  );
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Color(0xFF224957)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+                child: const Text('+'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
